79815bd0855110a9a6a7bd0894cb5bff5fedf2ee
Luke Wilkins <lukegwilkins@gmail.com>
2017-05-02 10:22:46 +0100

    last push

--- projectJavaSource/ClosureComparator.java
+++ projectJavaSource/ClosureComparator.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class ClosureComparator implements Comparator<ArrayList<String>>{
-	
+	//method that is used to sort the list of closures
 	@Override
 	public int compare(ArrayList<String> listOne, ArrayList<String> listTwo){
 		return listTwo.size()-listOne.size();
